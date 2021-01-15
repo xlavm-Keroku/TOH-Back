fa2354d714f9cb69754b09ac134778594f3aee13
Luis Angel Vanegas Matinez <angelvamart@hotmail.com>
2021-01-15 15:50:38 -0500

    change full

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 
 var express = require('express');
+var app = express();
 var cors = require('cors');
 const mongoose = require('mongoose');
-var app = express();
+require("dotenv").config();
+
 
 var port = process.env.PORT || 3000;
 const mongoURI = 'mongodb+srv://xlavm:0987654321xlavm@heroes-cluster.b2voe.mongodb.net/heroes?retryWrites=true&w=majority';
 
-mongoose.connect(mongoURI,{ useNewUrlParser: true, useUnifiedTopology: true })
-.then(() => console.log('MongoDB Connected'))
-.catch(err => console.log(err));
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false, }),);
@@ -19,7 +21,4 @@ const heroRoutes = require('./routes/hero.route')
 app.use('/hero', heroRoutes)
 
 
-app.listen(port, function() {
-    console.log('Server is running on port: ' + port);
-})
-
+app.listen(port, () => console.log(`Server running in port ${port}`)); 
