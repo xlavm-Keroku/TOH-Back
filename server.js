8fa57607afdf6babf7fa498f3293a9158514ee1c
Luis Angel Vanegas Matinez <angelvamart@hotmail.com>
2021-01-15 16:00:53 -0500

    change full Original

--- server.js
+++ server.js
@@ -1,24 +1,26 @@
-
-var express = require('express');
-var app = express();
-var cors = require('cors');
-const mongoose = require('mongoose');
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
 require("dotenv").config();
+const cors = require('cors')
 
-
-var port = process.env.PORT || 3000;
-const mongoURI = 'mongodb+srv://xlavm:0987654321xlavm@heroes-cluster.b2voe.mongodb.net/heroes?retryWrites=true&w=majority';
-
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('MongoDB Connected'))
-    .catch(err => console.log(err));
+const port = 3000
+const PATH_MONGO = "mongodb+srv://xlavm:0987654321xlavm@heroes-cluster.b2voe.mongodb.net/heroes?retryWrites=true&w=majority"
+const PATH_API_HERO = "/api/v1/hero"
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false, }),);
 app.use(cors())
 
-const heroRoutes = require('./routes/hero.route')
-app.use('/hero', heroRoutes)
+mongoose.connect(PATH_MONGO, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB Connected!'))
+    .catch(err => console.log(err))
 
+
+app.get('/', (req, res) => {res.send("Welcome to the API Back!")})
+
+
+const heroRoutes = require('./routes/hero.route')
+app.use(PATH_API_HERO, heroRoutes)
 
 app.listen(port, () => console.log(`Server running in port ${port}`)); 
