df09394fdb572db81d9da7f1824bf7bd7bc39023
Luis Angel Vanegas Matinez <angelvamart@hotmail.com>
2021-01-15 15:16:25 -0500

    constant

--- server.js
+++ server.js
@@ -4,13 +4,15 @@ const mongoose = require('mongoose')
 require("dotenv").config();
 const cors = require('cors')
 
-var port = process.env.PORT
+const port = 3000
+const PATH_MONGO = "mongodb+srv://xlavm:0987654321xlavm@heroes-cluster.b2voe.mongodb.net/heroes?retryWrites=true&w=majority"
+const PATH_API_HERO = "/api/v1/hero"
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false, }),);
 app.use(cors())
 
-mongoose.connect(process.env.PATH_MONGO, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(PATH_MONGO, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB Connected!'))
     .catch(err => console.log(err))
 
@@ -19,6 +21,6 @@ app.get('/', (req, res) => {res.send("Welcome to the API Back!")})
 
 
 const heroRoutes = require('./routes/hero.route')
-app.use(process.env.PATH_API_HERO, heroRoutes)
+app.use(PATH_API_HERO, heroRoutes)
 
 app.listen(port, () => console.log(`Server running in port ${port}`)); 
